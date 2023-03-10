89bf7423e8b2b86dd755315872bb2e8d65f5ea4c
zero8coder <s812419396@gmail.com>
2023-03-10 14:29:09 +0800

    fix:修复登录后跳转到 /threads 报错

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@ Route::get('/', [ThreadsController::class, 'index']);
 Route::get('/threads/create', [ThreadsController::class, 'create']);
 Route::get('/threads/{channel}/{thread}', [ThreadsController::class, 'show']);
 Route::post('/threads/{channel}/{thread}/replies', [RepliesController::class, 'store']);
-Route::get('/threads/{channel}', [ThreadsController::class, 'index']);
+Route::get('/threads/{channel?}', [ThreadsController::class, 'index']);
 Route::post('/threads', [ThreadsController::class, 'store']);
 //Route::resource('threads','ThreadsController');
