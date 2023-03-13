f51e76bee7e21848f2950e9ddd1cc543e5e377ee
zero8coder <s812419396@gmail.com>
2023-03-13 08:40:29 +0800

    feature:点赞回复

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Reply;
+use Illuminate\Support\Facades\DB;
+
+class FavoritesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Reply $reply)
+    {
+        return $reply->favorite();
+    }
+}
---
+++ tests/Feature/FavoritiesTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Reply;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FavoritiesTest extends TestCase
+{
+    /**
+     * @test
+     * 游客不能点赞回复
+     *
+     */
+    public function guests_can_not_favorite_anything()
+    {
+        $this->post('replies/1/favorites')
+            ->assertRedirect('/login');
+    }
+
+    /**
+     * @test
+     * 用户可以点赞任意回复
+     */
+    public function an_authenticated_user_can_favorite_any_reply()
+    {
+        $this->signIn();
+        $reply = Reply::factory()->create();
+        // 如果点赞了回复
+        $this->post('replies/' . $reply->id . '/favorites')->assertStatus(201);
+        // 会有一条记录在数据库
+        $this->assertCount(1, $reply->favorites);
+    }
+
+    /**
+     * @test
+     * 用户不能重复点赞
+     */
+    public function an_authenticated_user_may_only_favorite_a_reply_once()
+    {
+        $this->signIn();
+        $reply = Reply::factory()->create();
+        try{
+            $this->post('replies/' . $reply->id . '/favorites')->assertStatus(201);
+            $this->post('replies/' . $reply->id . '/favorites')->assertStatus(200);
+        } catch (\Exception $e) {
+            $this->fail('用户不能重复点赞');
+        }
+
+        $this->assertCount(1, $reply->favorites);
+    }
+}
