578bfb293d0945b0bf500e854431b05f4a7913ac
zero8Coder <s812419396@gmail.com>
2023-03-24 09:27:21 +0800

    fix:修复导航栏

--- resources/js/components/NavView.vue
+++ resources/js/components/NavView.vue
@@ -93,7 +93,7 @@
                             </MenuItems>
                         </transition>
                     </Menu>
-                    <div class="flex">
+                    <div v-else class="flex">
                         <a href="/login" class="flex text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
                             登录
                         </a>
